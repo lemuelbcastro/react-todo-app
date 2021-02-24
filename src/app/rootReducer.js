1aefa5e7aee086010e242c0e4f192780ae1fdc8e
Lemuel Castro <lemuelbercasiocastro@gmail.com>
2021-02-24 11:11:48 +0800

    Add todo slice

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authenticationReducer from '../features/Authentication/authenticationSlice';
 import spinnerReducer from '../features/Spinner/spinnerSlice';
+import todosReducer from '../features/Todos/todosSlice';
 import usersReducer from '../features/Users/usersSlice';
 
 export default combineReducers({
   authentication: authenticationReducer,
   spinner: spinnerReducer,
+  todos: todosReducer,
   users: usersReducer,
 });
